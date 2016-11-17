9cd75c1971e606538737e4032e040161033603e3
Jonathan Bisson <bjonnh@bjonnh.net>
2016-11-16 21:37:35 -0600

    The new circuit is done differently (because I don't know how to count pins on a 8 pin IC)

--- driver.py
+++ driver.py
@@ -2,7 +2,8 @@ import hid
 import time
 import random
 import math
-
+import pyaudio
+import numpy as np
 
 while True:
     try:
@@ -58,7 +59,7 @@ def sine_blend():
             color_fade = [int((1+math.sin(angle*((i+2)/7.0)))*c/2) for c in color]
 
             device.write([0x02]+ color_fade + [i] + [ 0, 0 ,0]);
-            time.sleep(0.0001);
+            time.sleep(0.01);
         angle += math.pi/40
 
 def rand_color(scale=50):
@@ -113,8 +114,107 @@ def northern_lights(brightness=50):
             time.sleep(0.001);
         angle += math.pi/18#36
 
-        
-northern_lights(150)
+
+def fire(brightness=50, force=255):
+    angle=0
+    color=[]
+    color_fade=[]
+    device.write([0xf0,brightness,0x00,0x00,0,0,0,0,0])
+    little_force = int((force+1)/10)
+    for led in range(0,8):
+        basic_tint = random.randint(0,force)
+        col = [basic_tint,
+               random.randint(0,little_force),
+               0]
+        color.append(col)
+        color_fade.append(col)
+    while True:
+        if random.random()>0.9:
+            force += 1
+            force = force%255
+            little_force = int((force+1)/5) %255
+            for j in range(0,8):
+                color[j] = [int(color[j][0] + random.randint(-int(force/5),int(force/5)))%force,
+                            int(color[j][1] + random.randint(-int(force/10),int(force/10)))%little_force,
+                            0
+                ]
+        for i in range(0,8):
+            ri = i#int(350*(1+math.sin(angle/1)))%8
+            old_fade = color_fade[ri]
+            color_fade[i] = [int((1+math.sin(angle*((i**2+2)/7.0)))*c/2) for c in color[i]]
+            color_fade[i] = [int((old_fade[0] + color_fade[i][0]/10)/1.1),
+                             int((old_fade[1] + color_fade[i][1]/10)/1.1),
+                             0]
+
+            device.write([0x02]+ color_fade[i] + [ri] + [ 0, 0 ,0]);
+            time.sleep(0.001);
+        angle += (force/64)*math.pi/64#36
+
+
+class SpectrumAnalyzer:
+    FORMAT = pyaudio.paFloat32
+    CHANNELS = 1
+    RATE = 44100
+    CHUNK = 512
+    START = 0
+    N = 512
+
+    wave_x = 0
+    wave_y = 0
+    spec_x = 0
+    spec_y = 0
+    data = []
+    led = 0
+    def __init__(self):
+        self.pa = pyaudio.PyAudio()
+        device.write([0xf0,0xff,0x00,0x00,0,0,0,0,0])
+        self.stream = self.pa.open(format = self.FORMAT,
+            channels = self.CHANNELS, 
+            rate = self.RATE, 
+            input = True,
+            output = False,
+            frames_per_buffer = self.CHUNK)
+        # Main loop
+        self.loop()
+    def loop(self):
+        try:
+            while True :
+                self.data = self.audioinput()
+                self.fft()
+                self.graphplot()
+
+        except KeyboardInterrupt:
+            self.pa.close()
+
+        print("End...")
+
+    def audioinput(self):
+        ret = self.stream.read(self.CHUNK)
+        ret = np.fromstring(ret, np.float32)
+        return ret
+
+    def fft(self):
+        self.wave_x = range(self.START, self.START + self.N)
+        self.wave_y = self.data[self.START:self.START + self.N]
+        self.spec_x = np.fft.fftfreq(self.N, d = 1.0 / self.RATE)  
+        y = np.fft.fft(self.data[self.START:self.START + self.N])    
+        self.spec_y = [np.sqrt(c.real ** 2 + c.imag ** 2) for c in y]
+    def graphplot(self):
+        self.led += 1
+        self.led = self.led%8
+        bass = int(np.sum(self.spec_y[0:170])/17)
+        medium = int(np.sum(self.spec_y[170:340])/2)
+        treble = int(np.sum(self.spec_y[340:512])/17)
+        print("{:03} {:03} {:03}".format(bass,medium,treble))
+        device.write([0x02,bass%255,medium%255,treble%255,
+                      self.led,0,0,0])
+              #print()
+#rotary_blend()        
+#sine_blend()
+#northern_lights(255)
+fft=SpectrumAnalyzer()
+
+# fire(255, 32) fire is dirty and buggy don't use it
 
 #device.write([0x04,0x01,0x01,0,0,0,0,0])
 
